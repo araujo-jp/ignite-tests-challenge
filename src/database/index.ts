5f16b63cca4d0ebcd157dd53299cd3728ebfa973
Joao Paulo Araujo <paulointer077@gmail.com>
2021-11-18 23:28:32 -0400

    feat: setting database connection for development

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,14 +1,14 @@
-import { Connection, createConnection, getConnectionOptions } from 'typeorm';
+import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-export default async (host = 'database-pg'): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
+
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
       database:
-        process.env.NODE_ENV === 'test'
-          ? 'fin_api'
+        process.env.NODE_ENV === "test"
+          ? "fin_api_test"
           : defaultOptions.database,
-    }),
+    })
   );
 };
